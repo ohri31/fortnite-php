59208550841d4400d3956c9b10fe5b0ebfc341aa
Mirza <mirza.ohranovic@gmail.com>
2020-02-18 18:32:21 +0100

    Avoid lookup deprecated endpoint

--- src/Stats.php
+++ src/Stats.php
@@ -25,7 +25,8 @@ class Stats {
      * @param string $access_token OAuth2 Access token
      * @param string $account_id   Epic account id
      */
-    public function __construct($access_token, $account_id) {
+    public function __construct($access_token, $account_id) 
+    {
         $this->access_token = $access_token;
         $this->account_id = $account_id;
         $data = $this->fetch($this->account_id);
@@ -39,38 +40,87 @@ class Stats {
      * @param  string $account_id   Account id
      * @return object               The stats data
      */
-    private function fetch($account_id) {
-        if (!$account_id) return null;
-
-        $data = FortniteClient::sendFortniteGetRequest(FortniteClient::FORTNITE_API . 'stats/accountId/' . $account_id . '/bulk/window/alltime',
-                                                     $this->access_token);
-
-        // Remove - from account ID and get it's display name
-        $this->display_name = Account::getDisplayNameFromID(str_replace("-","",$this->account_id), $this->access_token);
-        //if (!count($data)) throw new StatsNotFoundException('Unable to find any stats for account id '. $account_id);
-
-        // Loop over all the stat objects and compile them together cleanly
-        $compiledStats = [];
-        foreach ($data as $stat) {
-            $parsed = $this->parseStatItem($stat);
-            $compiledStats = array_merge_recursive($compiledStats, $parsed);
+    private function fetch($account_id) 
+    {
+        // if the accoutn ID is not set yet
+        if(!$account_id) {
+            return null;
         }
 
-        // Now loop over the compiled stats and create proper objects
-        $platforms = [];
-        foreach ($compiledStats as $key => $platform) {
-            $platforms[$key] = new Platform($platform);
-        }
+        try {
+            // endpoint initilization 
+            $endpointStats = FortniteClient::FORTNITE_API . 'stats/accountId/' . $account_id . '/bulk/window/alltime';
 
-        return $platforms;
+            // initilize data & display name 
+            $data = FortniteClient::sendFortniteGetRequest($endpointStats, $this->access_token);
+            $this->display_name = Account::getDisplayNameFromID(str_replace("-", "", $account_id), $this->access_token);
+
+            // initilize compiled and platforms arrays 
+            $compiled = [];
+            $platforms = [];
+
+            // loop over all the stat object and compile them 
+            foreach ($data as $stat) {
+                $parsed = $this->parseStatItem($stat);
+                $compiled = array_merge_recursive($compiled, $parsed);
+            }
+
+            // loop over the c ompiled and map them to platofrms
+            foreach ($compiled as $key => $platform) {
+                $platforms[$key] = new Platform($platform);
+            }
+
+            // return te platforms data
+            return $platforms;
+        } catch(\Exception $e) {
+            throw new UserNotFoundException("Unable to find the stats for this user.");
+        } 
     }
+
+    /**
+     * Lookup by the User account ID 
+     *
+     * @param string $account_id 
+     */
+    public function lookup($account_id) 
+    {
+        return new self(
+            $this->access_token, 
+            $account_id
+        );
+    }
+
+    /**
+     * Parses a stat string into a mapped array.
+     * @param  string $stat The stat string
+     * @return array        The mapped stat array
+     */
+    private function parseStatItem($stat): array 
+    {
+        //
+        // Example stat name:
+        // br_placetop5_ps4_m0_p10
+        // {type}_{name}_{platform}_{??}_{mode (squads/solo/duo)}
+        // 
+        $result = [];
+        $pieces = explode("_", $stat->name);
+        $result[$pieces[2]][$pieces[4]][$pieces[1]] = $stat->value;
+        return $result;
+    }
+
+    /** 
+     * NOTE (18/02/2020): 
+     *
+     * The Lookup API Endpoint got deprecated in 02/20 - so that one can't be used any more. 
+     * The Fetch stats was expanded in order to cover the correspoding exceptions * 
+     */
 
     /**
      * Lookup a user by their Epic display name.
      * @param  string $username Display name to search
      * @return object           New instance of Fortnite\Stats
      */
-    public function lookup($username) {
+    /* public function lookup($username) {
         try {
             $data = FortniteClient::sendFortniteGetRequest(FortniteClient::FORTNITE_PERSONA_API . 'public/account/lookup?q=' . urlencode($username),
                                                         $this->access_token);
@@ -91,27 +141,5 @@ class Stats {
             if ($e->getResponse()->getStatusCode() == 404) throw new UserNotFoundException('User ' . $username . ' was not found.');
             throw $e; //If we didn't get the user not found status code, just re-throw the error.
         }
-    }
-
-    /**
-     * Parses a stat string into a mapped array.
-     * @param  string $stat The stat string
-     * @return array        The mapped stat array
-     */
-    private function parseStatItem($stat): array {
-        //
-        // Example stat name:
-        // br_placetop5_ps4_m0_p10
-        // {type}_{name}_{platform}_{??}_{mode (squads/solo/duo)}
-        // 
-        $result = [];
-        $pieces = explode("_", $stat->name);
-        $result[$pieces[2]][$pieces[4]][$pieces[1]] = $stat->value;
-        return $result;
-    }
-
-    public function accountId() {
-        return $this->account_id;
-    }
-
+    } */
 }
